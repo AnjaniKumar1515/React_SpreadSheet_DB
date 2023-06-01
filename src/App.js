bdf33fa730de09979b350da389a76e8c6b3f5b73
AnjaniRai <hritek001@gmail.com>
2023-06-01 12:06:18 +0530

    React_Sheets_DB

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [data, setData] = useState([])
+  
+const handleClick = ()=>{
+    fetch('https://sheetdb.io/api/v1/tjn1sipgmfd3h')
+  .then((response) => response.json())
+  .then((data) =>setData(data));}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Fetch Data</button>
+      {data.map((items)=>(
+        <div key={items.id}> {items.Email_Address} </div>
+      ))}
     </div>
   );
 }
